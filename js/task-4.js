664e15eefe279cccaa7968066211560d8503442e
Daryna <buriak.daryna@gmail.com>
2024-12-14 20:18:56 +0200

    add task 4 - js

--- js/task-4.js
+++ js/task-4.js
@@ -4,3 +4,24 @@
 Якщо при сабміті у формі є незаповнені поля, виводь alert з попередженням про те, що 'All form fields must be filled in'. Не додавай на інпути атрибут required, валідація має відбуватися саме через JS.
 Якщо користувач заповнив усі поля і відправив форму, збери значення полів в об'єкт з двома властивостями, де ключ — це ім'я інпутів, а значення — відповідні значення цих інпутів, очищені від пробілів по краях. Для доступу до елементів форми використовуй властивість elements.
 При сабміті форми виведи об'єкт із введеними даними в консоль і очисти значення полів форми методом reset.*/
+
+const loginform = document.querySelector(".login-form");
+loginform.addEventListener("submit", handleSubmit);
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+  const login = form.elements.email.value.trim();
+  const password = form.elements.password.value.trim();
+
+  if (login === "" || password === "") {
+    return alert("All form fields must be filled in");
+  }
+
+  const user = {
+    login: login,
+    password: password,
+  };
+
+  console.log(user);
+  form.reset();
+}
