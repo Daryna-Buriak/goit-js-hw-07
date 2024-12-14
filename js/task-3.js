fcac8d8ded10884b93b28a9b0a45bec1b03fbda5
Daryna <buriak.daryna@gmail.com>
2024-12-14 18:23:21 +0200

    add task 3 - js

--- js/task-3.js
+++ js/task-3.js
@@ -1,1 +1,11 @@
-/*Напиши скрипт, який під час набору тексту в інпуті input#name-input (подія input) підставляє його поточне значення в span#name-output як ім’я для привітання. Обов’язково очищай значення в інпуті по краях від пробілів . Якщо інпут порожній або містить лише пробіли, то замість імені у спан має підставлятися рядок "Anonymous".*/
+/*Напиши скрипт, який під час набору тексту в інпуті input#name-input (подія input) підставляє його поточне значення в span#name-output як ім’я для привітання. Обов’язково очищай значення в інпуті по краях від пробілів. Якщо інпут порожній або містить лише пробіли, то замість імені у спан має підставлятися рядок "Anonymous".*/
+
+const textInput = document.querySelector("#name-input");
+const output = document.querySelector("#name-output");
+textInput.addEventListener("input", (event) => {
+  if (textInput.value.trim() === "") {
+    output.textContent = "Anonymous";
+  } else {
+    output.textContent = event.currentTarget.value.trim();
+  }
+});
